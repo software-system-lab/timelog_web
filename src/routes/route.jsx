2daa0952e98030098da6ac20aa363c9e3a34aaa3
Regina <ycycchre@gmail.com>
2020-06-20 18:18:33 +0800

    route

--- src/routes/route.jsx
+++ src/routes/route.jsx
@@ -6,12 +6,12 @@ import Secured from 'views/Secured';
 import Profile from 'views/Profile';
 
 const appRoutes = [
-  {path: "/welcome", name:"Welcome", component: Welcome},
   {path: "/board", name: "Board", component: Board},
   {path: "/history", name: "History", component: History},
   {path: "/activity", name: "Activity", component: Activity},
   {path: "/profile", name: "Profile", component: Profile},
   {path: "/secured", name:"Secured", component: Secured},
+  {path: "/", name:"Welcome", component: Welcome},
 ]
 
 export default appRoutes;

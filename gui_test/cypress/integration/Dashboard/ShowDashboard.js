b7ce7f2f579828345382839ab83ae890c5f7b471
OIS team <ssl1321ois@gmail.com>
2020-11-11 10:22:14 +0800

    board cypress

--- gui_test/cypress/integration/Dashboard/ShowDashboard.js
+++ gui_test/cypress/integration/Dashboard/ShowDashboard.js
@@ -1,29 +1,31 @@
-import {have_value, address} from '../../util/commands';
+import {have_value} from '../../util/commands';
+import {address} from '../../util/constant';
 
 describe("ShowDashboard",() => {
 
-    const addLogBtn       = '//div[@id="root"]//li[1]//button[1]';
-    const title           = '//input[@id="title"]';
-    const activityType    = '//div[@id="activity-type-select"]';
-    const labProject      = '//li[contains(text(),"LabProject")]';
-    const startDateBox    = '//div[@class="MuiPaper-root MuiDialog-paper MuiDialog-paperScrollPaper MuiDialog-paperWidthSm MuiPaper-elevation24 MuiPaper-rounded"]//div[1]//div[1]//div[1]//div[1]//input[1]';
-    const startTimeBox    = '//div[@class="MuiDialogContent-root"]//div[2]//div[1]//div[1]//div[1]//input[1]';
-    const endDateBox      = '//div[@class="MuiGrid-root MuiGrid-container MuiGrid-spacing-xs-3"]//div[3]//div[1]//div[1]//div[1]//input[1]';
-    const endTimeBox      = '//div[4]//div[1]//div[1]//div[1]//input[1]';
-    const date_7          = '//div[@class="MuiPickersSlideTransition-transitionContainer MuiPickersCalendar-transitionContainer"]//div[2]//div[3]//button[1]';
-    const hourAM          = '//span[@class="MuiButton-label"]/h6[text()="AM"]';
-    const hourPM          = '//span[@class="MuiButton-label"]/h6[text()="PM"]';
-    const clock           = '//div[@class="MuiPickersClock-squareMask"]';
-    const description     = '//input[@id="description"]';
-    const submitBtn       = '//span[contains(text(),"Submit")]';
+
+    const durationBtn       = '//body/div[@id="root"]/div[1]/div[1]/nav[1]/div[1]/div[1]/div[1]/ul[1]/div[1]/li[3]/button[1]';
+    const startDateBox      = '//div[@class="MuiPaper-root MuiDialog-paper MuiDialog-paperScrollPaper MuiDialog-paperWidthSm MuiPaper-elevation24 MuiPaper-rounded"]//div[1]//div[1]//div[1]//div[1]//label[contains(text(),"Start")]';
+    const startDate20201104 = '//div[@class="MuiPaper-root MuiDialog-paper MuiDialog-paperScrollPaper MuiDialog-paperWidthSm MuiPaper-elevation24 MuiPaper-rounded"]//div[1]//div[1]//div[1]//div[1]//input[@value="2020/11/04"]';
+    const endDateBox        = '//div[@class="MuiPaper-root MuiDialog-paper MuiDialog-paperScrollPaper MuiDialog-paperWidthSm MuiPaper-elevation24 MuiPaper-rounded"]//div[1]//div[2]//div[1]//div[1]/input[1]';
+    const yearBox           = '//h6[contains(text(),"20")]';
+    const year2020          = '//div[contains(text(),"2020")]';
+    const startMonth        = '//p[@class="MuiTypography-root MuiTypography-body1 MuiTypography-alignCenter"]';
+    const chooseUpMonth     = '//div[@class="MuiPickersCalendarHeader-switchHeader"]//button[1]';
+    const date_4            = '//div[@class="MuiPickersSlideTransition-transitionContainer MuiPickersCalendar-transitionContainer"]//div[1]//div[1]//div[4]';
+    const date_10           = '//div[@class="MuiPickersSlideTransition-transitionContainer MuiPickersCalendar-transitionContainer"]//div[1]//div[2]//div[3]';
+    const durationSubmit    = '//span[contains(text(),"Submit")]';
+
+    const boardBtn          = '//body/div[@id="root"]/div[1]/div[1]/nav[1]/div[1]/div[1]/div[1]/ul[2]/div[1]';
+    const filterBtn         = '//body/div[@id="root"]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/button[1]';
 
     
     it("Login to the website",()=>{  
 
         
-        const userName        = '//input[@id="username"]';
-        const password        = '//input[@id="password"]';
-        const loginBtn        = '//input[@id="kc-login"]';
+        const userName        = '//input[@id="username-field"]';
+        const password        = '//input[@id="password-field"]';
+        const loginBtn        = '//input[@id="login-button"]';
 
         cy.visit(address);
         //-------------login---------------
@@ -38,10 +40,64 @@ describe("ShowDashboard",() => {
         //click login
         cy.xpath(loginBtn).click();
         //----------------------------------
-        
-
-        
     });
+
+    it("Go to Dashboard",()=>{
+        cy.ClickTo(boardBtn);
+
+    });
+    
+    it("Set Duration",()=>{  
+
+        cy.ClickTo(durationBtn);
+        cy.ClickTo(startDateBox);
+        cy.ClickTo(yearBox);
+        cy.ClickTo(year2020);
+        cy.document().then(document =>{
+            var j = 0;
+            let mon;
+            do
+            {
+                cy.xpath(startMonth).then(($month) => {
+                    mon = $month.text();
+                    if(mon != 'November 2020'){
+                        cy.ClickTo(chooseUpMonth);
+                    }
+                });
+                j++;
+            }
+            while(j < 12);  
+        })
+        cy.ClickTo(date_4);
+        cy.xpath(startDate20201104)
+          .should(have_value,"2020/11/04");
+
+        cy.ClickTo(endDateBox);
+        cy.ClickTo(yearBox);
+        cy.ClickTo(year2020);
+        cy.document().then(document =>{
+            var j = 0;
+            let mon;
+            do
+            {
+                cy.xpath(startMonth).then(($month) => {
+                    mon = $month.text();
+                    if(mon != 'November 2020'){
+                        cy.ClickTo(chooseUpMonth);
+                    }
+                });
+                j++;
+            }
+            while(j < 12);  
+        })
+        cy.ClickTo(date_10);
+        cy.xpath(endDateBox)
+          .should(have_value,"2020/11/10");
+        cy.ClickTo(durationSubmit);
+
+    });
+
+    
     
 
     // it.skip("Go To History",()=>{
